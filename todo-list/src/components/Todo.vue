00a446c2f7cb7b300fdf7f5da7f2306df346e675
Parth Sharma <parthsharma@trios.com>
2020-01-28 17:01:28 -0800

    todo list

---
+++ todo-list/src/components/Todo.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h1> This is my todo list</h1>
+
+        <div class="row">
+            <div class="col-lg-12">
+                <input type="text" class="form-control" v-model="task"> <button @click="add"> Add </button>
+            </div>
+            <br><br>
+            <h3>There are {{tasks.length}} items in your list</h3>
+
+
+            <ul>
+                <li v-for="(t,index) in tasks" :key="index">
+                    {{t}}
+                </li>
+            </ul>
+
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default{
+
+        data(){
+
+            return{
+               task :'',
+               tasks:[]
+            }
+        },
+
+        methods:{
+
+            add(){
+
+                this.tasks.push(this.task)
+                this.task = ""
+
+            }
+        }
+
+
+    }
+
+</script>
