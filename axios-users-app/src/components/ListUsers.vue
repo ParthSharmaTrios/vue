f5c54cb0f06aa3436c2c6fcb7ae018d84de34011
Parth Sharma <parthsharma@trios.com>
2020-01-30 17:16:28 -0800

    vue axios

---
+++ axios-users-app/src/components/ListUsers.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <div>
+
+        <b-list-group>
+            <b-list-group-item v-for="(user,index) in users" :key="index">
+               {{user.name}}
+            </b-list-group-item>
+        </b-list-group>
+
+    </div>
+
+</template>
+
+<script>
+
+    const axios = require('axios');
+    export default{
+
+        data(){
+           return {
+                users:[]
+           }
+        },
+
+        created() {
+            this.fetchUsers()
+        },
+
+        methods:{
+
+            fetchUsers(){
+
+
+                axios.get('http://jsonplaceholder.typicode.com/users')
+                    .then(response => {
+                        this.users = response.data
+                    })
+            }
+
+        }
+    }
+</script>
--- todo-list/src/components/Todo.vue
+++ todo-list/src/components/Todo.vue
@@ -57,6 +57,8 @@
 <script>
     export default{
 
+
+
         data(){
 
             return{
